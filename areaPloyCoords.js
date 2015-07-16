aad3ca3a98ee73da33c737c309037475c0a296bf
yamagao <yamagao@126.com>
2015-07-16 16:36:16 -0400

    use array instead of single variables

--- areaPloyCoords.js
+++ areaPloyCoords.js
@@ -92,49 +92,54 @@ var FloridaCoords = [
 	new google.maps.LatLng(30.9682, -87.5912),
 	new google.maps.LatLng(30.9964, -87.6050)
 ];
-var AlachuaCoords = [
-	new google.maps.LatLng(29.821583,-82.647400),
-	new google.maps.LatLng(29.933515,-82.534790),
-	new google.maps.LatLng(29.835879,-82.114563),
-	new google.maps.LatLng(29.709525,-82.051392),
-	new google.maps.LatLng(29.444381,-82.062378),
-	new google.maps.LatLng(29.499378,-82.196960),
-	new google.maps.LatLng(29.504159,-82.551270),
-	new google.maps.LatLng(29.547177,-82.556763),
-	new google.maps.LatLng(29.547177,-82.655640)
-];
-var BradfordCoords = [
-	new google.maps.LatLng(30.143940,-82.142029),
-	new google.maps.LatLng(29.931135,-82.415314),
-	new google.maps.LatLng(29.907329,-82.408447),
-	new google.maps.LatLng(29.838261,-82.114563),
-	new google.maps.LatLng(29.714296,-82.048645),
-	new google.maps.LatLng(30.147502,-82.048645),
-	new google.maps.LatLng(30.145127,-82.139282)
-];
+var countyCoords = {	
+	"counties": [
+		{"name": "Alachua", "coords": [
+			new google.maps.LatLng(29.821583,-82.647400),
+			new google.maps.LatLng(29.933515,-82.534790),
+			new google.maps.LatLng(29.835879,-82.114563),
+			new google.maps.LatLng(29.709525,-82.051392),
+			new google.maps.LatLng(29.444381,-82.062378),
+			new google.maps.LatLng(29.499378,-82.196960),
+			new google.maps.LatLng(29.504159,-82.551270),
+			new google.maps.LatLng(29.547177,-82.556763),
+			new google.maps.LatLng(29.547177,-82.655640)
+		]}
+		,
+		{"name": "Bradford", "coords": [
+			new google.maps.LatLng(30.143940,-82.142029),
+			new google.maps.LatLng(29.931135,-82.415314),
+			new google.maps.LatLng(29.907329,-82.408447),
+			new google.maps.LatLng(29.838261,-82.114563),
+			new google.maps.LatLng(29.714296,-82.048645),
+			new google.maps.LatLng(30.147502,-82.048645),
+			new google.maps.LatLng(30.145127,-82.139282)
+		]}
+	]
+}
 
 // Construct the polygon.
 var Florida = new google.maps.Polygon({
 	paths: FloridaCoords,
 	strokeColor: '#D4886A',
 	strokeOpacity: 0.8,
-	strokeWeight: 2,
+	strokeWeight: 4,
 	fillColor: '#FFC2AA',
-	fillOpacity: 0.15
+	fillOpacity: 0.1
 });
 var Alachua = new google.maps.Polygon({
-	paths: AlachuaCoords,
+	paths: countyCoords.counties[0].coords,
 	strokeColor: '#D4886A',
 	strokeOpacity: 0.8,
 	strokeWeight: 2,
 	fillColor: '#FFC2AA',
-	fillOpacity: 0.35
+	fillOpacity: 0.2
 });
 var Bradford = new google.maps.Polygon({
-	paths: BradfordCoords,
+	paths: countyCoords.counties[1].coords,
 	strokeColor: '#D4886A',
 	strokeOpacity: 0.8,
 	strokeWeight: 2,
 	fillColor: '#FFC2AA',
-	fillOpacity: 0.35
+	fillOpacity: 0.2
 });
