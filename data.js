76a0c36d5ed7fdd36a6fe95f2e626a9c5711f2f2
yamagao <yamagao@126.com>
2015-07-16 15:16:21 -0400

    markers done!

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var data = {
 	"counties": [
-		{"name": "Alachua", "contact": "Annie Sheldon", "extension": "UF/IFAS Extension Alachua County UF East Campus", "address": "2046 N.E. Waldo Road, Suite 3221, Gainesville, FL 32609", "email": "aasheldon@ufl.edu", "latitude": 29.6706703, "longitude": -82.299}
+		{"name": "Alachua", "contact": "Annie Sheldon", "extension": "UF/IFAS Extension Alachua County, UF East Campus", "address": "2046 N.E. Waldo Road, Suite 3221, Gainesville, FL 32609", "phone": "", "email": "aasheldon@ufl.edu", "latitude": 29.6706703, "longitude": -82.299}
 		,
 		{"name": "Bradford", "contact": "Samara Deary", "extension": "UF/IFAS Extension Bradford County", "address": "2266 N. Temple Ave., Starke, FL 32091", "phone": "(904) 966-6224", "email": "sdeary@ufl.edu", "latitude": 29.9671377, "longitude": -82.1031321}
 		,
@@ -22,7 +22,7 @@ var data = {
 		,
 		{"name": "Flagler", "contact": "Sharon Treen", "extension": "UF/IFAS Extension Flagler County", "address": "150 Sawgrass Road, Bunnell, FL 32110", "phone": "(386) 437-7464", "email": "streen@ufl.edu", "latitude": 29.4884095, "longitude": -81.2771561}
 		,
-		{"name": "Franklin", "contact": "Melanie Taylor", "extension": "UF/IFAS Extension Franklin County", "address": "66 Fourth St., Apalachicola, FL 32320-1775", "email": "metaylor@ufl.edu", "latitude": 29.7263884, "longitude": -84.9848686}
+		{"name": "Franklin", "contact": "Melanie Taylor", "extension": "UF/IFAS Extension Franklin County", "address": "66 Fourth St., Apalachicola, FL 32320-1775", "phone": "", "email": "metaylor@ufl.edu", "latitude": 29.7263884, "longitude": -84.9848686}
 		,
 		{"name": "Gilchrist", "contact": "Holly Houghton", "extension": "UF/IFAS Extension Gilchrist County", "address": "125 E. Wade St., Trenton, FL 32693", "phone": "(352) 463-3174", "email": "hvh@ufl.edu", "latitude": 29.613656, "longitude": -82.817369}
 		,
@@ -39,6 +39,39 @@ var data = {
 		{"name": "Madison", "contact": "Diann Douglas", "extension": "UF/IFAS Extension Madison County", "address": "184 N.W. College Loop, Madison, FL 32340", "phone": "(850) 973-4138", "email": "ddoug@ufl.edu", "latitude": 30.4820355, "longitude": -83.4052956}
 		,
 		{"name": "Martin", "contact": "Chris Kilbride", "extension": "UF/IFAS Extension Martin County", "address": "2614 S.E. Dixie Hwy., Stuart, FL 34996", "phone": "(772) 288-5654", "email": "kilbride@ufl.edu", "latitude": 27.1691102, "longitude": -80.2228595}
-
+		,
+		{"name": "Nassau", "contact": "Meg McAlpine", "extension": "UF/IFAS Extension Nassau County", "address": "543350 U.S. Hwy. 1, Callahan, FL 32011", "phone": "(904) 530-6350", "email": "connor@ufl.edu", "latitude": 30.531697, "longitude": -81.8575819}
+		,
+		{"name": "Okaloosa", "contact": "Jill Breslawski", "extension": "UF/IFAS Extension Okaloosa County", "address": "3098 Airport Road, Crestview, FL 32539", "phone": "(850) 689-5850", "email": "jbreslawski@ufl.edu", "latitude": 30.793858, "longitude": -86.533467}
+		,
+		{"name": "Orange", "contact": "Kim Shouldice", "extension": "UF/IFAS Extension Orange County", "address": "6021 S. Conway Road, Orlando, FL 32812", "phone": "(407) 254-9200", "email": "kshouldice@ufl.edu", "latitude": 28.4682121, "longitude": -81.3300692}
+		,
+		{"name": "Osceola", "contact": "Grisel Negron", "extension": "UF/IFAS Extension Osceola County", "address": "1921 Kissimmee Valley Lane", "phone": "(321) 697-3000", "email": "gnegron@ufl.edu", "latitude": 28.2964929, "longitude": -81.3657206}
+		,
+		{"name": "Pasco", "contact": "Betsy Crisp", "extension": "UF/IFAS Extension Pasco County, Central Pasco Professional Center", "address": "4111 Land O' Lakes Blvd., Suite 314, Land O' Lakes, FL 34639", "phone": "(813) 929-2725", "email": "mecrisp@ufl.edu", "latitude": 28.2172337, "longitude": -82.4592657}
+		,
+		{"name": "Pinellas", "contact": "Nan Jensen", "extension": "UF/IFAS Extension Pinellas County", "address": "12520 Ulmerton Road, Largo, FL 33774", "phone": "(727) 582-2104", "email": "njensen@ufl.edu", "latitude": 27.8836364, "longitude": -82.8086862}
+		,
+		{"name": "Putnam", "contact": "Wendy Lynch", "extension": "UF/IFAS Extension Putnam County", "address": "111 Yelvington Road, Suite 1, East Palatka, FL 32131", "phone": "(386) 329-0318", "email": "wendyw74@ufl.edu", "latitude": 29.642937, "longitude": -81.588515}
+		,
+		{"name": "St. Johns", "contact": "Joanne Cooper", "extension": "UF/IFAS Extension St. Johns County", "address": "3125 Agricultural Center Drive, St Augustine, FL 32092", "phone": "(904) 209-0430", "email": "jgcoop@ufl.edu", "latitude": 29.904896, "longitude": -81.409951}
+		,
+		{"name": "St. Lucie", "contact": "Karla Lenfesty", "extension": "UF/IFAS Extension St. Lucie County", "address": "8400 Picos Road, Suite 101, Ft. Pierce, FL 34945", "phone": "(772) 462-1660", "email": "lenfesty@ufl.edu", "latitude": 27.4330659, "longitude": -80.41145}
+		,
+		{"name": "Santa Rosa", "contact": "Ginny Hinton", "extension": "UF/IFAS Extension Santa Rosa County", "address": "6263 Dogwood Drive, Milton, FL 32570", "phone": "(850) 623-3868", "email": "ginnyh@santarosa.fl.gov", "latitude": 30.662751, "longitude": -87.0554494}
+		,
+		{"name": "Sarasota", "contact": "Amber Ward", "extension": "UF/IFAS Extension Sarasota County", "address": "6700 Clark Road, Sarasota, FL 34241", "phone": "(941) 861-9804", "email": "alward@ufl.edu", "latitude": 27.2672574, "longitude": -82.4393364}	
+		,
+		{"name": "Suwannee", "contact": "Cathy Rogers", "extension": "UF/IFAS Extension Suwannee County", "address": "1302 11th St. S.W., Live Oak, FL 32064", "phone": "(386) 362-2771", "email": "cjrogers@ufl.edu", "latitude": 30.2911031, "longitude": -83.0058548}
+		,
+		{"name": "Taylor", "contact": "Lori Wiggins", "extension": "UF/IFAS Extension Taylor County", "address": "203 Forest Park Drive, Perry, FL 32348", "phone": "(850) 838-3508", "email": "lwiggins@ufl.edu", "latitude": 30.0797595, "longitude": -83.5676256}	
+		,
+		{"name": "Union", "contact": "Samara Deary", "extension": "UF/IFAS Extension Union County", "address": "25 N.E. 1St St., Lake Butler, FL 32054", "phone": "(386) 496-2321", "email": "sdeary@ufl.edu", "latitude": 30.0238759, "longitude": -82.3381309}
+		,
+		{"name": "Volusia", "contact": "Jill Taufer", "extension": "UF/IFAS Extension Volusia County", "address": "3100 E. New York Ave., Deland, FL 32724", "phone": "(386) 822-5778", "email": "jtaufer@ufl.edu", "latitude": 29.016363, "longitude": -81.223067}	
+		,
+		{"name": "Wakulla", "contact": "Shelley Swenson", "extension": "UF/IFAS Extension Wakulla County", "address": "84 Cedar Ave., Crawfordville, FL 32327", "phone": "(850) 926-3931", "email": "sswenson@ufl.edu", "latitude": 30.186232, "longitude": -84.3787229}
+		,
+		{"name": "Walton", "contact": "Ricki McWilliams", "extension": "UF/IFAS Extension Walton County", "address": "732 North 9th St., DeFuniak Springs, FL 32433", "phone": "(850) 892-8172", "email": "rickim@ufl.edu", "latitude": 30.7457466, "longitude": -86.114714}			
 	]
 }
